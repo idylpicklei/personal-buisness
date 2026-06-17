7293d8f367311ecd9c589107c02d27b4cd873204
DYLAN\kdroo <dylan.collom@kdroofing.com>
2026-06-17 10:34:26 -0600

    update Copyright

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,7 +4,7 @@ export function SiteFooter() {
   return (
     <footer className="border-t border-white/10 px-6 py-10 lg:px-8">
       <div className="mx-auto flex max-w-7xl flex-col gap-6 text-sm text-zinc-400 md:flex-row md:items-center md:justify-between">
-        <p>Copyright &copy; 2026 DC Ventures LLC. All rights reserved.</p>
+        <p>Copyright &copy; 2026 DC LLC. All rights reserved.</p>
         <div className="flex flex-wrap gap-x-6 gap-y-3">
           {footerLinks.map((link) => (
             <a
